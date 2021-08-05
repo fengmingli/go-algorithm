1c94714ba38da61f6cce933bc98256560607398c
lifengming <lifengming@mockuai.com>
2021-08-06 00:10:09 +0800

    fix

--- leetcode/list/00025.go
+++ leetcode/list/00025.go
@@ -2,18 +2,23 @@
 * @Author LFM
 * @Date 2021/8/5 10:36 下午
 * @Since V1
-*/
+ */
 
 package list
 
+import (
+	"go-algorithm/structures"
+)
 
-//
-////reverseKGroup  https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
-//
-//func reverseKGroup(head *ListNode) *ListNode {
-//	dummy := &ListNode{Next: head}
-//	for pt := dummy; pt != nil && pt.Next != nil && pt.Next.Next != nil; {
-//		pt, pt.Next, pt.Next.Next, pt.Next.Next.Next = pt.Next, pt.Next.Next, pt.Next.Next.Next, pt.Next
-//	}
-//	return dummy.Next
-//}
+// Node ListNode define
+type Node = structures.ListNode
+
+//reverseKGroup  https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+
+func reverseKGroup(head *Node) *Node {
+	dummy := &Node{Next: head}
+	for pt := dummy; pt != nil && pt.Next != nil && pt.Next.Next != nil; {
+		pt, pt.Next, pt.Next.Next, pt.Next.Next.Next = pt.Next, pt.Next.Next, pt.Next.Next.Next, pt.Next
+	}
+	return dummy.Next
+}
